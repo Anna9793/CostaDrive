9d8f1b1f4e36288fd6419e5989dae8f14ce995ee
Anna9793 <anna.w.bielska@gmail.com>
2026-07-07 12:10:27 +0200

    Fichero para administrar tablas en interfaz admin de Django

--- CostaDrive/CostaDriveApp/admin.py
+++ CostaDrive/CostaDriveApp/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Reservas, Incidencias, Vehiculos, Clientes
+
+@admin.register(Reservas)
+class ReservasAdmin(admin.ModelAdmin):
+    list_display = ('id_reserva', 'id_cliente', 'id_vehiculo', 'fecha_inicio', 'estado_reserva')
+    list_filter = ('estado_reserva', 'fecha_inicio')
+    search_fields = ('id_reserva', 'id_cliente__nombre', 'id_cliente__dni')
+    ordering = ('-fecha_reserva',)
+
+@admin.register(Incidencias)
+class IncidenciasAdmin(admin.ModelAdmin):
+    list_display = ('id_incidencia', 'id_reserva', 'fecha_incidencia', 'coste', 'estado_incidencia', 'destino_coste')
+    list_filter = ('estado_incidencia', 'destino_coste', 'fecha_incidencia')
+    search_fields = ('id_incidencia', 'descripcion')
+    list_editable = ('estado_incidencia', 'destino_coste')
+
+@admin.register(Vehiculos)
+class VehiculosAdmin(admin.ModelAdmin):
+    list_display = ('matricula', 'marca', 'modelo', 'estado_vehiculo', 'precio_dia')
+    list_filter = ('estado_vehiculo', 'marca')
+    search_fields = ('matricula', 'marca', 'modelo')
+
+@admin.register(Clientes)
+class ClientesAdemin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellidos', 'dni', 'telefono')
+    search_fields = ('nombre', 'apellidos', 'dni', 'email')
